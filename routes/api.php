e5868637d60934d15c4873cc0f7caaa5dab7e7d0
alexander Pena <alexhellhammer@hotmail.com>
2023-07-03 12:14:27 -0500

    mis ejemplos Endpoints

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//  Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//      return $request->user();
+//  });
+
+Route::get("/saludo",function (Request $request) {
+    $message = ["mensaje" => "hola mundo!"];
+    return response() -> json($message);
+});
+
+Route::put("/libro",function (Request $request) {
+    $message = ["book" => "Cronica de una muerte anunciada",
+                "año de creacion"=>"1985",
+                "autor" => " gabriel garcia marquez"];
+    return response() -> json($message);
+});
+Route::post("/cerveza",function (Request $request) {
+    $message = ["beer" => "Club colombia"];
+    return response() -> json($message);
+});
+Route::patch("/computador",function (Request $request) {
+    $message = ["computer" => "Toshiba"];
+    return response() -> json($message);
+});
+Route::delete("/musica",function (Request $request) {
+    $message = ["musik" => "bad bunny"];
+    return response() -> json($message);
 });
